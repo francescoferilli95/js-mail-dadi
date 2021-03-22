0c3e08c33198183210cd9ec5bd0546c6d1120191
francescoferilli95 <francesco.ferilli1995@libero.it>
2021-03-22 14:49:27 +0100

    adding js for email section

---
+++ js/main.js
@@ -0,0 +1,65 @@
+console.log('Hello Console!');
+
+/**
+ * JS EMAIL TO DO LIST:
+ *  1. ASK TO THE VISITOR FOR HIS EMAIL.
+ *  2. LOOP CHECK FOR THIS EMAIL TO BE IN THE LIST.
+ *  3. OUTPUT OF THE LOOP CHECK BOTH CASES TRUE OR FALSE.
+ * 
+ */
+
+// CREATING ARRAY OF KNOWN EMAIL LIST
+
+var accounts = ['tizio.caio@gmail.com', 'davide.rossi@virgilio.it', 'andrea.sempronio.libero.it', 'francesco.ferilli@alice.it', 'randomuser.nonloso@gmail.com'];
+// console.log(accounts);
+// console.log(accounts.length);
+
+
+// ASK FOR THE VISITOR FOR HIS EMAIL
+
+var userEmail = prompt('Please enter your Email Address here: ');
+// console.log(userEmail);
+
+// VARIABLE TO CHECK THE PRESENCE OF THE USEREMAIL INSIDE THE LIST
+var userEmailFound = false;
+
+// LOOP CHECK FOR THIS EMAIL TO BE IN THE LIST
+
+for (var i = 0; i < accounts.length; i++) {
+
+    // console.log(accounts[i]);
+
+    if (accounts[i] === userEmail) {
+        userEmailFound = true;
+        break;
+    }
+}
+
+    if (userEmailFound) {
+        console.log('Welcome ' + accounts[i] + ' you can Log In!');
+    } else {
+        console.log('Authentication Error, please register your Email before trying to Log In.');   
+    }
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * GIOCO DEI DADI TO DO LIST:
+ *  1. GENERATE RANDOM NUMBER BETWEEN 1 AND 6 FOR THE PLAYER.
+ *  2. GENERATE RANDOM NUMBER BETWEEN 1 AND 6 FOR THE COMPUTER.
+ *  3. DETERMINE THE WINNER BASED TO HIGHER RANDOM NUMBER.
+ *  4. OUTPUT OF THE RESULT OF THE GAME.
+ * 
+ */
+
+
